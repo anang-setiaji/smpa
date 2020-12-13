018b4f5c1a635cec2b4b807819c591571d3ef3b1
Anang Setiaji <58649077+anang-setiaji@users.noreply.github.com>
2020-12-13 20:36:21 +0800

    Update admin.blade.php

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -10,7 +10,7 @@
       <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <script src="js/main.js"></script>
   <script src="{{ asset('js/app.js') }}" defer></script>
-  <script src="https://smpa-chat.herokuapp.com/"></script>
+  <script src="https://smpa-chat.herokuapp.com/socket.io/socket.io.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
